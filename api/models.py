d9984576d6add36e0464cfc14ff0af96b3ac725d
Adham Salama <adhamsalama@zohomail.com>
2021-07-18 03:40:58 +0200

    Add models and serializers for Users and Passwords

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    pass
+
+class Password(models.Model):
+    email = models.EmailField(null=True)
+    username = models.CharField(max_length=64, null=True)
+    encrypted_password = models.CharField(max_length=256)
+    link = models.URLField(max_length=256, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='passwords')
---
+++ api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import User, Password
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'passwords']
+
+
+class PasswordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Password
+        fields = ['email', 'username', 'encrypted_password', 'link', 'user']
