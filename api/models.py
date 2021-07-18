abb84039f949ca7f84eef51fda68875267f30d6e
Adham Salama <adhamsalama@zohomail.com>
2021-07-18 13:23:53 +0200

    Make email and username optional

--- api/models.py
+++ api/models.py
@@ -7,8 +7,8 @@ class User(AbstractUser):
     pass
 
 class Password(models.Model):
-    email = models.EmailField(null=True)
-    username = models.CharField(max_length=64, null=True)
+    email = models.EmailField(null=True, blank=True)
+    username = models.CharField(max_length=64, null=True, blank=True)
     encrypted_password = models.CharField(max_length=256)
     url = models.URLField(max_length=256, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='passwords')
