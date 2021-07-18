e13675ddae56fffc936b40c2350c44ddbdde9c65
Adham Salama <adhamsalama@zohomail.com>
2021-07-18 04:04:38 +0200

    Add api and rest_framework to installed apps and specify AUTH_USER_MODEL

--- password_manager/settings.py
+++ password_manager/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'api',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'api.User'
