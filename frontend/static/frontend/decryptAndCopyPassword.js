981aacefff4be5294eb57875b8821a3d7634930f
Adham Salama <adhamsalama@zohomail.com>
2021-07-18 12:50:03 +0200

    Create a function for decrypting passwords and copying them to clipboard

---
+++ frontend/static/frontend/decryptAndCopyPassword.js
@@ -0,0 +1,22 @@
+function decryptAndCopyPassword() {
+    let copyButtons = document.querySelectorAll(".copy-button");
+    copyButtons.forEach(button => {
+        button.onclick = () => {
+        let encrypted_password = button.parentElement.querySelector(".encrypted-password").innerHTML.trim();
+        let MPW = prompt("Enter master password for decryption.");
+        let res = postData('http://127.0.0.1:8000/api/verify-master-password', data={master_password: MPW});
+        res.then(response => response.json()).then(result => {
+            if(result == false)
+                alert("Wrong Master Password.")
+            
+            else {
+                let decrypted_password = decrypt(encrypted_password, MPW);
+                navigator.clipboard.writeText(decrypted_password);
+            }    
+            
+        });
+         
+
+    }
+})
+}
