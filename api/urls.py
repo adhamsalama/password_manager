77027388e406bceb23104cd0fcc0785c41f4cbd2
Adham Salama <adhamsalama@zohomail.com>
2021-07-18 04:01:01 +0200

    Use custom IsOwner permissions and remove unimportant view classes and urls

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('passwords', views.PasswordList.as_view(), name='password-list'),
-    path('passwords/<int:pk>', views.PasswordDetail.as_view(), name='password-detail'),
-    path('users', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>', views.UserDetail.as_view(), name='user-detail')
+    path('api/passwords', views.PasswordList.as_view(), name='password-list'),
+    path('api/passwords/<int:pk>', views.PasswordDetail.as_view(), name='password-detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,13 +1,18 @@
 from .models import User, Password
 from .serializers import UserSerializer, PasswordSerializer
 from rest_framework import generics
+from .permissions import IsOwner
+from rest_framework import permissions
 
 # Create your views here.
 
 class PasswordList(generics.ListCreateAPIView):
     queryset = Password.objects.all()
     serializer_class = PasswordSerializer
-
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
+    def get_queryset(self):
+        return Password.objects.filter(user=self.request.user)
+    
     def perform_create(self, serializer):
         return serializer.save(user=self.request.user)
 
@@ -15,13 +20,4 @@ class PasswordList(generics.ListCreateAPIView):
 class PasswordDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Password.objects.all()
     serializer_class = PasswordSerializer
-
-
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
