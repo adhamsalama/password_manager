ee60f0ed17b8fddcdf2cc478c44e238436e8030d
Adham Salama <adhamsalama@zohomail.com>
2021-07-18 08:43:12 +0200

    Add urls for registeration, logging in and logging out

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('passwords', views.PasswordList.as_view(), name='password-list'),
     path('passwords/<int:pk>', views.PasswordDetail.as_view(), name='password-detail'),
+    path('login', views.LoginUser.as_view(), name='login-user'),
+    path('logout', views.LogoutUser.as_view(), name='logout-user'),
+    path('register', views.RegisterUser.as_view(), name='register-user')
 ]
