e990e901a07f635e13204b2d5cdf776f0d106cbc
Adham Salama <adhamsalama@zohomail.com>
2021-07-19 21:50:08 +0200

    Search for passwords only when pressing valid keys

--- frontend/static/frontend/searchPasswords.js
+++ frontend/static/frontend/searchPasswords.js
@@ -3,6 +3,9 @@ let form = document.querySelector("#search-form");
 form.onsubmit = () => false;
 
 let searchField = document.querySelector("#q");
-searchField.onkeyup = () => {
-    getPasswords(searchField.value).then(results => displayPasswords(results));
+searchField.onkeyup = (key) => {
+    // Search passwords only when entering characters, numbers, symbols
+    // or deleting a character by pressing backspace
+    if (key.key.length == 1 || key.code == "Backspace")
+        getPasswords(searchField.value).then(results => displayPasswords(results));
 }
