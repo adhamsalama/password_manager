be2e289caf1e599f5f03ca117cc00ceeb116463d
Adham Salama <adhamsalama@zohomail.com>
2021-07-21 15:49:31 +0200

    Remove password element after deleting it instead of reloading all passwords

--- frontend/static/frontend/deletePasswordEvent.js
+++ frontend/static/frontend/deletePasswordEvent.js
@@ -3,15 +3,15 @@ function deletePasswordEvent() {
     let deleteButtons = document.querySelectorAll(".delete-button");
     for (let i = 0; i < deleteButtons.length; i++) {
         deleteButtons[i].onclick = () => {
-            let id = deleteButtons[i].parentElement.parentElement.parentElement.id;
+            let parentDiv = deleteButtons[i].parentElement.parentElement.parentElement;
             const request = new Request(
-                `http://127.0.0.1:8000/api/passwords/${id}`,
+                `http://127.0.0.1:8000/api/passwords/${parentDiv.id}`,
                 { headers: { 'X-CSRFToken': getCookie('csrftoken'), 'content-type': 'application/json' } }
             );
             fetch(request, {
                 method: 'DELETE',
                 mode: 'same-origin'  // Do not send CSRF token to another domain.
-            }).then(() => getPasswords(document.querySelector("#q").value).then(results => displayPasswords(results)))
+            }).then(() => parentDiv.remove())
         }
     }
 }
